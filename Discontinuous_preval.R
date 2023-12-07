d082c9b722e5fa23d163db231857dcc78512b8c6
MarionThemeze-Leroy <themezeleroym@gmail.com>
2023-12-07 20:57:13 +0100

    Creation H-gopher$H
    Pour disconinuous_prevak : Ajout des librairies et du code d'importation des données

--- Discontinuous_preval.R
+++ Discontinuous_preval.R
@@ -1,4 +1,17 @@
 ### Model with discontinuous prevalence ### 
+
+## load libraries ##
+library(tidyverse)
+library(cowplot)
+library(R2jags)
+
+## import dataset
+gopher <- read.csv(file = "./gopher.csv", header = TRUE, sep = ";", dec=".", stringsAsFactors = TRUE)
+gopher$year<-as.factor(gopher$year) # set year as factor
+gopher$standprev<-(gopher$prev-mean(gopher$prev))/sd(gopher$prev) #standardize prev 
+gopher$H<-ifelse(gopher$prev>= 25,1,0) #tranform prev as discontinuous with H=0 if prev<25% and H=1 if prev>=25%
+
+
 set.seed(2023)
 gopher_discont_prev <-function(){
   for (i in 1:n){
@@ -30,4 +43,8 @@ M2 <- jags(data=datax,
            n.thin=1)
 
 M2
+
+#
 traceplot(M2,mfrow=c(3,1),varname=c('mu','beta','deviance'),ask=FALSE)
+head(M2$BUGSoutput$sims.matrix)
+acf(M2$BUGSoutput$sims.matrix[,2])

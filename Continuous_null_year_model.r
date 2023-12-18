4c3254c2e82a0ed61ecc79857aac4552e91f3e9c
BasilePajot <91027316+BasilePajot@users.noreply.github.com>
2023-12-18 11:45:26 +0100

    New model and rename

---
+++ Continuous_null_year_model.r
@@ -0,0 +1,76 @@
+# Import libraries
+require("anyLib")
+anyLib(c("tidyverse", "R2jags", "lme4", "cowplot"))
+
+# Set random seed
+set.seed(2023)
+
+# Data importation
+gopher <- read.csv("gopher.csv", header=TRUE, stringsAsFactors=TRUE, sep=";", dec=".") %>% 
+  mutate(standprev = (prev - mean(prev)) / sd(prev),
+         Cov.y.1 = ifelse(year == 2005, 1, 0),
+         Cov.y.2 = ifelse(year == 2006, 1, 0))
+
+# The null model function
+null_year_model <- function(){
+  # The null model does not take into account any of the variables. We just want to see the evolution of the number of shells
+  # Likelihood
+  for (i in 1:N){  # Loop over years
+    S[i] ~ dpois(lambda[i])
+    log(lambda[i]) <- mu.0 + alpha.y.1 * Cov.y.1[i] + alpha.y.2 * Cov.y.2[i] + log(A[i])
+  }
+  # Priors
+  mu.0 ~ dnorm(0, 1/100)
+  alpha.y.1 ~ dnorm(0, 1/100)
+  alpha.y.2 ~ dnorm(0, 1/100)
+}
+
+# Now, we can make list of the data to use in the jags function
+datax <- list(
+  N = gopher$year  %>% 
+    length(),
+  S = gopher$shells,        # Number of shells
+  A = gopher$Area,          # Area offset
+  Cov.y.1 = gopher$Cov.y.1,  # Effect of the year 2005
+  Cov.y.2 = gopher$Cov.y.2   # Effect of the year 2006
+)
+
+# Parameters to estimate
+params <- c("mu.0", "alpha.y.1", "alpha.y.2")
+
+# Initialising the chains
+init1 <- list(mu.0 = -0.5, alpha.y.1=-0.5, alpha.y.2=-0.5)
+init2 <- list(mu.0 = 0.5, alpha.y.1=0.5, alpha.y.2=0.5)
+init <- list(init1, init2)
+
+# Define the iteration parameters
+nb.iterations <- 10000
+nb.burnin <- 100
+# Run the model using jags
+M0 <- jags(
+  data = datax,
+  inits = init,
+  parameters.to.save = params,
+  model.file = null_year_model,
+  n.chains = 2,
+  n.iter = nb.iterations,
+  n.burnin = nb.burnin,
+  n.thin = 1
+)
+
+M0
+
+traceplot(M0, mfrow=c(1, 3), ask=FALSE)
+par(mfrow = c(1, 1))
+
+res <- M0$BUGSoutput$sims.matrix %>% 
+  as.data.frame()
+hist(res$mu.0)
+hist(res$alpha.y.1)
+hist(res$alpha.y.2)
+
+# The mean number of turtle shells per individual is : 
+shells <- gopher$Area  * exp(res$mu.0 + res$alpha.y.1 * gopher$Cov.y.1 + res$alpha.y.2 * gopher$Cov.y.2)
+hist(shells)
+mean(shells)
+hist(gopher$shells)
--- Continuous_random_model.r
+++ Continuous_random_model.r
@@ -7,7 +7,9 @@ set.seed(2023)
 
 # Data importation
 gopher <- read.csv("gopher.csv", header=TRUE, stringsAsFactors=TRUE, sep=";", dec=".") %>% 
-  mutate(standprev = (prev - mean(prev)) / sd(prev))
+  mutate(standprev = (prev - mean(prev)) / sd(prev),
+         Cov.y.1 = ifelse(gopher$year == 2005, 1, 0),
+         Cov.y.2 = ifelse(gopher$year == 2006, 1, 0))
 
 # The random model
 random_model <- function(){
@@ -15,20 +17,18 @@ random_model <- function(){
   # Likelihood
   for(i in 1:N){
     S[i] ~ dpois(lambda[i])
-    log(lambda[i]) <- alpha.s[site[i]] + b.prev * prev[i] + mu.0 + alpha.y[year[i]] + log(A[i])
+    log(lambda[i]) <- mu.0 + alpha.s[site[i]] + b.prev * prev[i] + alpha.y.1 * Cov.y.1[i] + alpha.y.2 * Cov.y.2[i] + log(A[i])
   }
   # Priors
   mu.0 ~ dnorm(0, 1/100)
   b.prev ~ dnorm(0, 1/100)
-  for (k in 1:nb.years){
-    alpha.y[k] ~ dnorm(0, 1/100)
-  }
   for (j in 1:nb.sites){
-    alpha.s[j] ~ dnorm(mu.s, tau.s)
+    alpha.s[j] ~ dnorm(0, tau.s)
   }
-  mu.s ~ dnorm(0, 1/100)
   tau.s <- 1 / (sd.s * sd.s)
   sd.s ~ dunif(0, 100)
+  alpha.y.1 ~ dnorm(0, 1/100)
+  alpha.y.2 ~ dnorm(0, 1/100)
 }
 
 # Make the data to use in jags
@@ -43,29 +43,27 @@ datax <- list(
   nb.sites = gopher$Site %>% 
     unique() %>% 
     length(),
-  year = ifelse(gopher$year == 2004, 1, ifelse(gopher$year == 2005, 2, 3)),
-  nb.years = gopher$year %>% 
-    unique() %>% 
-    length()
+  Cov.y.1 = ifelse(gopher$year == 2005, 1, 0),
+  Cov.y.2 = ifelse(gopher$year == 2006, 1, 0)
 )
 
 # Make a list of parameters to save
-params = c("mu.0", "b.prev", "mu.s", "sd.s", "alpha.y")
+params = c("mu.0", "b.prev", "sd.s", "alpha.y.1", "alpha.y.2")
 
 # Initial conditions
 init1 <- list(
   "mu.0" = 0.5,
   "b.prev" = 0.5,
-  "mu.s" = 0.5,
   "sd.s" = 0.5,
-  "alpha.y" = rep(0.5, gopher$year %>% unique() %>% length())
+  "alpha.y.1" = 0.5,
+  "alpha.y.2" = 0.5
 )
 init2 <- list(
   "mu.0" = - 0.5,
   "b.prev" = - 0.5,
-  "mu.s" = - 0.5,
   "sd.s" =  0.25,
-  "alpha.y" = rep(-0.5, gopher$year %>% unique() %>% length())
+  "alpha.y.1" = -0.5,
+  "alpha.y.2" = -0.5
 )
 init <- list(init1, init2)
 
@@ -89,5 +87,18 @@ M2
 traceplot(M2, mfrow=c(2, 3), ask=FALSE)
 par(mfrow=c(1, 1))
 
-autocorr.plot(as.mcmc(M2), ask=FALSE)
-par(mfrow=c(1, 1))
+res <- M2$BUGSoutput$sims.matrix %>% 
+  as.data.frame()
+
+# Looking at the distribution
+hist(res$b.prev)
+hist(res$mu.0)
+hist(res$alpha.y.1)
+hist(res$alpha.y.2)
+hist(res$sd.s)
+
+# Calculating the mean number of shells per individual 
+shells <- gopher$Area * exp(res$mu.0 + res$b.prev * gopher$standprev + res$alpha.y.1 * gopher$Cov.y.1 + res$alpha.y.2 * gopher$Cov.y.2 + rnorm(1, mean=0, sd=res$sd.s))
+
+hist(shells)
+mean(shells)
